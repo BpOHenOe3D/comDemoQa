385f853f83f5157eade678729074f24d73430415
BpOHenOe3D <xkotrusx@gmail.com>
2022-08-04 18:23:22 +0300

    add Search JU5 example in Selenide Wiki

---
+++ src/test/java/selenideGitHub/SearchJU5.java
@@ -0,0 +1,33 @@
+package selenideGitHub;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchJU5 {
+    @BeforeAll
+
+    static void configure() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void Search() {
+        // Ищем репозиторий Селенида
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        // Проверяем наличие SoftAssertions в Вики Селенида и наличие примера кода JU5
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $(".Box--condensed").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class"));
+    }
+}
