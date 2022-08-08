5e6e84f7b368661a1ef050a0c47f11d555d5d7a9
BpOHenOe3D <xkotrusx@gmail.com>
2022-08-08 17:59:06 +0300

    added GDragAndDrop test

---
+++ src/test/java/pricingGitHub/DragAndDrop.java
@@ -0,0 +1,25 @@
+package pricingGitHub;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+    @BeforeAll
+
+    static void configure() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.holdBrowserOpen = true;
+    }
+   @Test
+    void move(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
